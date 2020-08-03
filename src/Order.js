99e5984ab5a14052d224f8a626e486a546355b0b
Roman Yarovoy <roman@booma.co.il>
2020-08-03 17:04:09 +0300

    order-fix

--- src/Order.js
+++ src/Order.js
@@ -4,16 +4,40 @@ import "react-datepicker/dist/react-datepicker.css";
 
 
 export class Order extends React.Component {
-  state = {
-    startDate: new Date()
-  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: new Date(),
+      minutes: '',
+      hours: ''
+    };
+
+    this.onChangeMin = this.onChangeMin.bind(this);
+    this.onChangeHours = this.onChangeHours.bind(this);
+  }
+
   handleChange = date => {
     this.setState({
-      startDate: date
+      startDate: date,
+      hours: '',
+      minutes: ''
     });
   };
+  onChangeMin = (event) => {
+    this.setState({
+      minutes: event.target.value
+    });
+  };
+  onChangeHours = (event) => {
+    this.setState({
+      hours: event.target.value
+    });
+  }
 
   render() {
+    console.log(this.state.minutes);
+    console.log(this.state.hours);
 
     return (
       <div>
@@ -24,7 +48,12 @@ export class Order extends React.Component {
         <br></br>
         <span>Введите дату</span>
         <DatePicker selected={this.state.startDate} onChange={this.handleChange} />
-        <span>Введите время</span>
+        <br></br>
+        <span>Введите часы</span>
+        <input type="text" name="hours" value={this.state.hours} onChange={this.onChangeHours}></input>
+        <br></br>
+        <span>Введите минуты</span>
+        <input type="text" name="minutes" value={this.state.minutes} onChange={this.onChangeMin}></input>
       </div>
     );
   }
